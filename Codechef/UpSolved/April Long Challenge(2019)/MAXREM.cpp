6cf2dbf942fae28f0adca71612415ef84b26110e
Aryan Barnwal <55250734+Aryan6290@users.noreply.github.com>
2020-07-27 17:49:40 +0530

    April 2019 Long

---
+++ Codechef/UpSolved/April Long Challenge(2019)/MAXREM.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main() {
+	int n;
+	cin >> n;
+	int a[n];
+	for(int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n, greater<int>());
+	unique(a, a + n);
+	cout << a[1] % a[0] << endl;
+	return 0;
+}
