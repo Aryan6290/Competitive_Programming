e80828e3dfd535a34f854cd29ced4363e127fafe
Aryan Barnwal <55250734+Aryan6290@users.noreply.github.com>
2020-07-28 19:04:59 +0530

    done

--- Codechef/Others/SAVKONO.cpp
+++ Codechef/Others/SAVKONO.cpp
@@ -5,23 +5,39 @@ int main(){
     cin>>t;
     while(t--){
     int n;
-    int p;
-    cin>>n>>p;
-    int A[n];
-    for(int i=0;i<n;i++){
-    	cin>>A[i];
-	}
-	sort(A,A+n);
-	int key=0;
-	while(p>0)
-	{
-	p=p-A[n-1];
-	A[n-1]=A[n-1]/2;
-	sort(A,A+n);
-	key++;
-	
-	}
-	cout<<key<<endl;
+    int z;
+    int x;
+    
+       cin>>n>>z;
+        int hash[10003];
+        memset(hash,0,sizeof(hash));
+        for(int i=0;i<n;i++)
+        {
+            cin>>x;
+            hash[x]++;
+        }
+        int ans=0;
+        for(int i=10002 ; i>0 ; i--)
+        {
+            while(hash[i] && z>0)
+            {
+                z=z-i;
+                hash[i]--;
+                hash[i/2]++;
+                ans++;
+            }
+            if(z<=0)
+            break;
+        }
+        
+        if(z<=0)
+        {
+            cout<<ans<<"\n";
+        }
+        else
+        {
+            cout<<"Evacuate"<<"\n";
+        }
     	
     }
 }
