dea0541e0253e82af8f5b205b34bfd067abce54f
Aryan Barnwal <55250734+Aryan6290@users.noreply.github.com>
2020-08-01 19:29:16 +0530

    Interactive Problems

---
+++ Codechef/Others/CHFINTRO.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int m;
+    int t;
+    cin>>t>>m;
+    while(t--){
+        int x;
+        cin>>x;
+    if(x<m)
+    {
+        cout<<"Bad boi"<<endl;
+    }
+    else
+    cout<<"Good boi"<<endl;
+        
+    }
+}
