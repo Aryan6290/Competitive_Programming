9478e119d4e585159a5a76ddceca0b4f619e41b4
Aryan Barnwal <55250734+Aryan6290@users.noreply.github.com>
2020-08-04 16:16:57 +0530

    Update SAKTAN.cpp

--- Codechef/Others/SAKTAN.cpp
+++ Codechef/Others/SAKTAN.cpp
@@ -1,42 +1,46 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-#define int long long int
-#define cases int testcases;cin>>testcases; while(testcases--)
-int Row[100005],Col[100005];
-int32_t main()
-{
-    cases
-    {
-        int n,m,q,x,y;
-        cin>>n>>m>>q;
-        for(int i=1;i<=n;i++)
-        {
-                Row[i]=0;
-        }
-        for(int i=1;i<=m;i++)
-        {
-                Col[i]=0;
-        }
-   
-        while(q--)
-        {
-            cin>>x>>y;
-            Row[x]++;
-            Col[y]++;
-            Row[x]%=2;Col[y]%=2;
-        }
-        int a=0,b=0,c=0,d=0;
-        for(int i=1;i<=n;i++)
-        {
-            if(Row[i]==1) a++;
-            else b++;
-        }
-        for(int i=1;i<=m;i++)
-        {
-            if(Col[i]==1)c++;else d++;
-        }
-        int res= (a*d + b*c);
-        cout<<res<<endl;
-    }
-    return 0;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int m,n;
+		int ans=0;
+		int q;
+		cin>>n>>m>>q;
+		int A[n][m];
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				A[i][j]=0;
+			}
+			
+			
+		}
+		
+		while(q--){
+			int x,y;
+			cin>>x>>y;
+			for(int i=0;i<m;i++){
+				A[x-1][i]++;
+			}
+			for(int j=0;j<n;j++){
+				A[j][y-1]++;
+			}
+		
+			
+		}
+			for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				if(A[i][j]%2!=0)
+				{
+					ans++;
+				}
+	//			cout<<A[i][j]<<" ";
+			}
+	//		cout<<endl;
+			
+		}
+		cout<<ans<<endl;
+		
+	}
 }
