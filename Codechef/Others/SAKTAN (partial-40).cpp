76234181512b8c4d4c1cf8df56a9dfead704fbb9
Aryan Barnwal <55250734+Aryan6290@users.noreply.github.com>
2020-08-04 14:58:10 +0530

    Create SAKTAN (partial-40).cpp

---
+++ Codechef/Others/SAKTAN (partial-40).cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int t;
+	cin>>t;
+	while(t--){
+		int m,n;
+		int ans=0;
+		int q;
+		cin>>n>>m>>q;
+		int A[n][m];
+		for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+				A[i][j]=0;
+			}
+			
+			
+		}
+		
+		while(q--){
+			int x,y;
+			cin>>x>>y;
+			for(int i=0;i<m;i++){
+				A[x-1][i]++;
+			}
+			for(int j=0;j<n;j++){
+				A[j][y-1]++;
+			}
+				for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+//				if(A[i][j]%2!=0)
+//				{
+//					ans++;
+//				}
+				cout<<A[i][j]<<" ";
+			}
+			cout<<endl;
+			
+		}
+			
+		}
+			for(int i=0;i<n;i++){
+			for(int j=0;j<m;j++){
+//				if(A[i][j]%2!=0)
+//				{
+//					ans++;
+//				}
+				cout<<A[i][j]<<" ";
+			}
+			cout<<endl;
+			
+		}
+		cout<<ans<<endl;
+		
+	}
+}
